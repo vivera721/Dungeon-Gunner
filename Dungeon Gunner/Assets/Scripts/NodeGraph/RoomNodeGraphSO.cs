20a4d5602beb7788a40e220c6a68284f5c7edeec
vivera721 <vivera721@gmail.com>
2022-06-10 14:20:53 +0900

    Store Details About Room Tilemaps & Create Rooms
    타일맵 prefab 만들고 각 방 만들기

--- Dungeon Gunner/Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
+++ Dungeon Gunner/Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "RoomNodeGraph", menuName = "Scriptable Object/Dungeon/Room Node Graph")]
+[CreateAssetMenu(fileName = "RoomNodeGraph", menuName = "Scriptable Objects/Dungeon/Room Node Graph")]
 public class RoomNodeGraphSO : ScriptableObject
 {
     [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
