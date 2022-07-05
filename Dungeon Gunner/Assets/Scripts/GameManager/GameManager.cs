5233a0ca4472a33fcc53fb8d1db286a45ae67bc8
vivera721 <vivera721@gmail.com>
2022-07-05 21:48:04 +0900

    Minimap
    미니맵 카메라 - (카메라, 시네머신), 미니맵 클래스, UI 미니맵 Texture 설정으로 플레이시 화면에 띄우기

--- Dungeon Gunner/Assets/Scripts/GameManager/GameManager.cs
+++ Dungeon Gunner/Assets/Scripts/GameManager/GameManager.cs
@@ -162,6 +162,14 @@ public class GameManager : SingletoneMonobehaviour<GameManager>
     }
 
     /// <summary>
+    /// Get the player minimap icon
+    /// </summary>
+    public Sprite GetPlayerMiniMapIcon()
+    {
+        return playerDetails.playerMiniMapIcon;
+    }
+
+    /// <summary>
     /// Get the current room the player is in
     /// </summary>
     public Room GetCurrentRoom()
