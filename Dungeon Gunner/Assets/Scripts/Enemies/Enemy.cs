bed058946969df9c24913328fd993f05633eb3d5
vivera721 <vivera721@gmail.com>
2022-07-07 00:46:06 +0900

    Enemy Setup
    각각의 적 기초 설정 - 프리팹 만들기, 기초 설정

---
+++ Dungeon Gunner/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+#region REQUIRE COMPONENT
+[RequireComponent(typeof(SortingGroup))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))] 
+#endregion
+
+[DisallowMultipleComponent]
+public class Enemy : MonoBehaviour
+{
+    [HideInInspector] public EnemyDetailsSO enemyDetails;
+    private CircleCollider2D circleCollider2D;
+    private PolygonCollider2D polygonCollider2D;
+    [HideInInspector] public SpriteRenderer[] spriteRendererArray;
+
+
+    private void Awake()
+    {
+        circleCollider2D = GetComponent<CircleCollider2D>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+        spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+}
